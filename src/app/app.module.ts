2c454c33a4177bb9ae8f7c52beebca118e185b39
[epozdnyakov] <[epozdnkv@mail.ru]>
2022-10-20 21:06:52 +0300

    Make project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyMenuComponent } from './my-menu/my-menu.component';
+import { HeroComponent } from './hero/hero.component';
+import { WorkComponent } from './work/work.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyMenuComponent,
+    HeroComponent,
+    WorkComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule
